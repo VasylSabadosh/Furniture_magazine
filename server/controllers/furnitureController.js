9c2d885a4699640dc02013ebdf951ac84a033d03
Vasyl <vasia.sabadosh@gmail.com>
2023-01-09 19:23:52 +0200

    server2

--- server/controllers/furnitureController.js
+++ server/controllers/furnitureController.js
@@ -1,17 +1,29 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Furniture} = require('../models/models')
+const {Furniture, FurnitureInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
+const { where } = require('underscore')
 
 class FurnitureController {
     async create(req, res, next) {
         try {
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
-
             const furniture = await Furniture.create({name, price, typeId, brandId, img: fileName})
+
+            if(info) {
+                info = JSON.parse(info)
+                info.forEach(i => {
+                    FurnitureInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        furnitureId: furniture.id
+                    })
+                })
+            }
+
 
             return res.json(furniture)
         } catch (e) {
@@ -21,11 +33,41 @@ class FurnitureController {
     }
 
     async getAll(req, res) {
+        let {brandId, typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit ||9
+        let offset = page*limit-limit
+        let furnitures;
+        if(!brandId && !typeId) {
+            furnitures = await Furniture.findAndCountAll(limit, offset)
+        }
 
+        if(brandId && !typeId) {
+            furnitures = await Furniture.findAndCountAll({where:{brankId}, limit, offset})
+
+        }
+
+        if(!brandId && typeId) {
+            furnitures = await Furniture.findAndCountAll({where:{typeId}, limit, offset})
+
+        }
+
+        if(brandId && typeId) {
+            furnitures = await Furniture.findAndCountAll({where:{brankId, typeId}, limit, offset})
+
+        }
+        return res.json(furnitures);
     }
 
     async getOne(req, res) {
-
+        const {id} = req.params
+        const furniture = await Furniture.findOne(
+            {
+                where: {id},
+                include: [{model: FurnitureInfo, as:'info'}]
+            },
+        )
+        return res.json(furniture)
     }
 
 }
--- server/routes/furnitureRouter.js
+++ server/routes/furnitureRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const furnitureController = require('../controllers/furnitureController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
 
-router.post('/', furnitureController.create)
+router.post('/', checkRole('ADMIN'), furnitureController.create)
 router.get('/', furnitureController.getAll)
 router.get('/:id', furnitureController.getOne)
 
